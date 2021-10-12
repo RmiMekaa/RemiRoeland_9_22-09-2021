de7da6e694c0ba0dbf95f333c378091bb3fdda7d
Rémi <roeland.remi@yahoo.fr>
2021-10-12 18:17:35 +0200

    writing test WIP

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -3,12 +3,23 @@ import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+// describe("Given I am connected as an employee", () => {
+//   describe("When I am on NewBill Page", () => {
+//     test("Then ...", () => {
+//       const html = NewBillUI()
+//       document.body.innerHTML = html
+//       //to-do write assertion
+//     })
+//   })
+// })
+
+describe("When I am on NewBill Page", () => {
+  describe("If I upload a file with invalid format", () => {
+    it ("should send an alert message", () => {
+      
+    })
+    it ("should clear the file input value", () => {
+      
     })
   })
 })

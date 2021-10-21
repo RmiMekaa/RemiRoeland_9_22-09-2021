f554b9041dc7ba047bc0469609f9a6a4325c47aa
Rémi <roeland.remi@yahoo.fr>
2021-10-21 23:28:45 +0200

    Add comments

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -19,15 +19,58 @@ const row = (bill) => {
     `)
   }
 
+/**
+ * [rows description]
+ *
+ * @typedef   {Object}  billData   les données d'une facture
+ *
+ * @property   {Number} amount          exemple : 348
+ * @property   {String} commentAdmin    exemple : ""
+ * @property   {String} commentary      exemple : "hjghjhjghj"
+ * @property   {String} date            exemple : "2222-12-12"
+ * @property   {String} email           exemple : "roeland.remi@yahoo.fr"
+ * @property   {String} fileName        exemple : "mockup.jpg"
+ * @property   {String} fileUrl         exemple : "https://firebasestorage.googleapis.com/v0/b/billable-677b6.appspot.com/o/justificatifs%2Fmockup.jpg?alt=media&token=ff10cf1c-c0e9-45e9-9948-872235edb49d"
+ * @property   {String} id              exemple : "KP7OezAd36WXHLzPydrD"
+ * @property   {String} name            exemple : "yuuytuytu"
+ * @property   {Number} pct             exemple : 20
+ * @property   {String} status          exemple : "refused"
+ * @property   {String} type            exemple : "Restaurants et bars"
+ * @property   {String} vat             exemple : ""
+ */
+
+/** 
+ *  les factures à afficher
+ *  @typedef {Array.<billData>}  bills
+ */
+
+/**
+ * [rows description]
+ *
+ * @param   {bills}  data  les factures
+ *
+ * @return  {String}       les factures triées et fusionnées avec un template (row)
+ */
 const rows = (data) => {
   const antiChrono = (a, b) => (new Date(b.date) - new Date(a.date));
-  if (data !== undefined)data.sort(antiChrono)
+  if (data !== undefined) data.sort(antiChrono)
   return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
+/**
+ * [default description]
+ *
+ * @param   {bills}   data     les facture à afficher
+ * @param   {Boolean} loading  [loading description]
+ * @param   {Object}  error    [error description]
+ *
+ * @return  {String}            le html de la page
+ *
+ */
 export default ({ data: bills, loading, error }) => {
+  
   const modal = () => (`
-    <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal fade" data-testid="proofModal" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header">
